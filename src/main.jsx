9d80ee72d826b371e0a40ce47478e4e2298a4be4
Saeed Mohamed <sydalshghty15@gmail.com>
2025-05-31 16:23:16 +0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { HashRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter basename={import.meta.env.BASE_URL}>
+  <HashRouter basename="/dashboard-unitedhangers">
     <App />
   </HashRouter>
 );
